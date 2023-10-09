526aaa716dc5a714875ef81bff2c239a992835a8
Ivan <dilkas@bk.ru>
2023-10-09 16:47:10 +0400

    TMP

--- Assets/Source/Camera/CameraFollower.cs
+++ Assets/Source/Camera/CameraFollower.cs
@@ -10,18 +10,13 @@ public class CameraFollower : MonoBehaviour
     {
         float radians = _angle * Mathf.Deg2Rad;
 
-        // Создать матрицу поворота вокруг оси X
         Matrix4x4 rotationMatrix = Matrix4x4.identity;
         rotationMatrix.SetRow(0, new Vector4(1, 0, 0, 0));
         rotationMatrix.SetRow(1, new Vector4(0, Mathf.Cos(radians), -Mathf.Sin(radians), 0));
         rotationMatrix.SetRow(2, new Vector4(0, Mathf.Sin(radians), Mathf.Cos(radians), 0));
 
-        // Перемножить матрицу поворота и исходный вектор
         Vector4 rotatedVector = rotationMatrix.MultiplyPoint(new Vector4(Vector3.forward.x, Vector3.forward.y, Vector3.forward.z, 1.0f));
-
-        // Получить результат в виде Vector3
         Vector3 rotatedVector3 = new Vector3(rotatedVector.x, rotatedVector.y, rotatedVector.z) * _height;
-
         Vector3 newPosition = _playerTansform.position + rotatedVector3;
         transform.position = newPosition;
     }
